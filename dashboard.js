fa94194790f5c07fb13af601cf88d652506f8e5f
david avishay landau <94964998+davidlandau770@users.noreply.github.com>
2025-06-29 17:01:01 +0300

    Add files via upload

---
+++ dashboard.js
@@ -0,0 +1,2 @@
+import { MAX_USERS, DEFAULT_ROLE } from "./userConfig.js";
+console.log(MAX_USERS, DEFAULT_ROLE);
---
+++ helpers.js
@@ -0,0 +1,11 @@
+function toUpper(text) {
+    return text.toUpperCase();
+}
+function toLower(text) {
+    return text.toLowerCase();
+}
+
+export {
+    toUpper,
+    toLower
+}
